f29b9bb6e39769905d727fdff2f672ec6e528cee
zhanggianluca <99273189+zhanggianluca@users.noreply.github.com>
2022-12-01 21:05:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,80 @@
 function initialize() {
     emojis = ["🍎","🍎","🍕","🍕","🍣","🍣","🌮","🌮","🌭","🌭","☕️","☕️","🍩","🍩","🥡","🥡"];
-    backSides = document.getElementsByClassName('card-back'); 
+    cardBacks = document.getElementsByClassName('card-back');
+    backSides = Array.from(cardBacks);  
     for (var i = 0; i < backSides.length; ++i) {
         var num = Math.floor(Math.random() * emojis.length); 
-        var side = backSides[i]; 
-        side.innerHTML = emojis[num];
+        backSides[i].innerHTML = emojis[num];
         emojis.splice(num, 1);  
+        backSides[i].style.backgroundColor = "#C5FFFD"; 
+        backSides[i].style.visibility = "hidden"; 
     }
     clicks = 0; 
-    disable = false;  
+    displayClicks();   
 }
 
 function flip(card) {
-    if (disable) {
-        clearTimeout(); 
+    cards = document.getElementsByClassName("card");
+    var back = cards[card-1].getElementsByClassName("card-back")[0]; 
+    var tmp = getComputedStyle(back).visibility; 
+    if (tmp == "hidden") {
+        cards[card-1].getElementsByClassName("card-back")[0].style.visibility = "visible";
     }
     else {
-        cards = document.getElementsByClassName("card");
-        var backSide = cards[card-1].getElementsByClassName("card-back")[0];  
-        var css = getComputedStyle(backSide); 
-        var tmp = css.visibility; 
-        if (tmp == "hidden") {
-            cards[card-1].getElementsByClassName("card-back")[0].style.visibility = "visible";
+        cards[card-1].getElementsByClassName("card-back")[0].style.visibility = "hidden";
+    } 
+
+    check();
+    if (count == 2) {
+        pair();
+    }   
+    clicks++; 
+    displayClicks();  
+}
+
+function check() {
+    count = 0; 
+    for (var i = 0; i < backSides.length; ++i) {
+        var visibility = getComputedStyle(backSides[i]).visibility; 
+        var color = getComputedStyle(backSides[i]).backgroundColor; 
+        if (visibility == "visible" || color == "#C5FFFD") {  
+            count++;  
         }
-        else {
-            cards[card-1].getElementsByClassName("card-back")[0].style.visibility = "hidden";
-        }
-        pair();     
-        clicks++; 
-        displayClicks(); 
     }
 }
 
 function pair() {
-    var count = 0; 
     cardPair = []; 
     for (var i = 0; i < backSides.length; ++i) {
         var visibility = getComputedStyle(backSides[i]).visibility; 
-        if (visibility == "visible") {
-            count++;  
+        var color = getComputedStyle(backSides[i]).backgroundColor; 
+        if (visibility == "visible" || color == "#C5FFFD") {  
             cardPair.push(backSides[i]); 
         }
     }
-    if (count >= 2) {
-        disable = true; 
-        compare();   
-    }
+    compare();   
 }
 
 function compare() {
     if (cardPair[0].innerHTML == cardPair[1].innerHTML) {
         displayPair();  
+        for (var i = 0; i < backSides.length; ++i) {
+            if (backSides[i] == cardPair[0] || backSides[i] == cardPair[1]) {
+                backSides.splice(i, 1); 
+                i--; 
+            }
+        } 
     }
-    else {
-        wrong();
-        cardPair[0].style.visibility = "hidden"; 
-        cardPair[1].style.visibility = "hidden";  
+    else { 
+        disable(); 
+        wrong();  
+        cardPair[0].style.backgroundColor = "#C5FFFD"; 
+        cardPair[1].style.backgroundColor = "#C5FFFD"; 
     }
-    disable = false; 
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
 }
 
 function displayPair() {
@@ -67,14 +84,22 @@ function displayPair() {
 }
 
 function wrong() {
-    cardPair[0].style.backgroundColor =  "#F35550";
-    cardPair[1].style.backgroundColor =  "#F35550";
+    delay(1000).then(() => cardPair[0].style.visibility = "hidden");
+    delay(1000).then(() => cardPair[1].style.visibility = "hidden"); 
+    delay(1000).then(() => enable()); 
 }
 
-  
-  
 function displayClicks() {
     numClicks = document.getElementById("score"); 
     numClicks.innerHTML = clicks; 
 }
 
+var holder; 
+function disable() {
+    if (!holder) holder = window.flip; 
+    window.flip = function(){}; 
+}
+
+function enable() {
+    window.flip = holder; 
+}
